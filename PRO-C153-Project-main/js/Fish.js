3ee5f46c9ed0258b8e4231b54decc20a0fb2ae94
Rishan <gw14islamrishan@glow.sch.uk>
2024-01-11 09:57:50 +0000

    Project 156

--- PRO-C153-Project-main/js/Fish.js
+++ PRO-C153-Project-main/js/Fish.js
@@ -1,27 +1,47 @@
-/*AFRAME.registerComponent("fish",{
-    init:function(){
-        for(var i = 1; i <= 20; i++){
-            const id = `fish${i}`
-
-            const posX = Math.random()*100+-50
-            const posY = Math.random()*5+5
-            const posZ = Math.random()*60+-40
-
-            const pos = {x:posX, y:posY, z:posZ}
-            this.createfishs(id, position)
-        }
+AFRAME.registerComponent("fish", {
+    init: function () {
+      for (var i = 1; i <= 10; i++) {
+        const id = `fish${i}`;
+  
+        const posX = Math.random() * 100 + -50;
+        const posY = Math.random() * 5 + 5;
+        const posZ = Math.random() * 60 + -40;
+        const position = { x: posX, y: posY, z: posZ };
+        this.genrateFish(id, position);
+      }
     },
-    createfishs:function(){
-        const obstacleEntity = document.querySelector("#island")
-        const fishEl = document.createElement("a-entity")
-        fishEl.setAttribute("id", id);
-        fishEl.setAttribute("position", position);
-        fishEl.setAttribute("scale", { x: 500, y: 500, z: 500 });
-        fishEl.setAttribute("gltf-model", "PRO-C153-Project-main/assets/fish/scene.gltf")
-        fishEl.setAttribute("game-play", {
-            elementId: `#${id}`,
-          });
-        obstacleEntity.appendChild(fishEl);
+    genrateFish: function (id, position) {
+      const fishModelEntity = document.querySelector("#fishModels");
+      var fishEl = document.createElement("a-entity");
+  
+      fishEl.setAttribute("id", id);
+      fishEl.setAttribute("position", position);
+      fishEl.setAttribute("rotation", { x: 0, y: 180, z: 0 });
+  
+      fishEl.setAttribute("scale", { x: 0.3, y: 0.3, z: 0.3 });
+  
+      fishEl.setAttribute("gltf-model", "./assets/fish/scene.gltf");
+  
+      
+      fishEl.setAttribute("animation", {
+        property: "position",
+        to: "100 10 -20",
+        loop: "true",
+        dur: 20000,
+      });
+  
+      fishEl.setAttribute("animation-mixer", {});
+  
 
+      fishEl.setAttribute("static-body", {
+        shape: "sphere",
+        sphereRadius: 2
+      });
+  
+       fishEl.setAttribute("game-play", {
+        elementId: `#${id}`,
+      });
+  
+      fishModelEntity.appendChild(fishEl);
     }
-})*/
+  });
--- PRO-C153-Project-main/js/Rotation.js
+++ PRO-C153-Project-main/js/Rotation.js
@@ -1,86 +1,49 @@
-//Diver rotation component
-AFRAME.registerComponent("island-rotation", {
-  schema: {
-    speedOfRotation: { type: "number", default: 0 },
-  },
-  init: function () {
-    window.addEventListener("keydown", (e) => {
-      if (e.key === "ArrowRight") {
-        if (this.data.speedOfRotation > -0.1) {
-          this.data.speedOfRotation -= 0.01;
-        }
-      }
-      if (e.key === "ArrowLeft") {
-        if (this.data.speedOfRotation < 0.1) {
-          this.data.speedOfRotation +=0.01 ;
-        }
-        
-      }
-    });
-  },
-  tick: function () {
-    var mapRotation = this.el.getAttribute("rotation");
-
-    mapRotation.y += this.data.speedOfRotation;
-
-    this.el.setAttribute("rotation", {
-      x: mapRotation.x,
-      y: mapRotation.y,
-      z: mapRotation.z,
-    });
-  },
-});
-
-
 AFRAME.registerComponent("diver-rotation-reader", {
   schema: {
-    speedOfRotation: { type: "number", default: 0 },
-    speedOfAscent: { type: "number", default: 0 }
+    speedOfRoation: { type: "number", default: 0 },
+    speedOfMovement: { type: "number", default: 0 }
   },
   init: function () {
     window.addEventListener("keydown", (e) => {
 
-    
-      this.data.speedOfRotation = this.el.getAttribute("rotation");      
-      this.data.speedOfAscent = this.el.getAttribute("position");
+      this.data.speedOfRoation = this.el.getAttribute("rotation");      
+      this.data.speedOfMovement = this.el.getAttribute("position");
 
-      var diverRotation = this.data.speedOfRotation;      
-      var diverPosition = this.data.speedOfAscent;
+      var diverRotation = this.data.speedOfRoation;      
+      var diverPosition = this.data.speedOfMovement;
 
-    
-      if (e.key === "ArrowRight") {
-        if (diverRotation.x < 10) {
-          diverRotation.x += 0.7;
-          this.el.setAttribute("rotation", diverRotation);
-        }
-      }
-      if (e.key === "ArrowLeft") {
-        if (diverRotation.x > -10) {
-          diverRotation.x -= 0.7;
-          this.el.setAttribute("rotation", diverRotation);
-        }
-      }
-      if (e.key === "ArrowUp") {
-        if (diverRotation.z < 10) {
-          diverRotation.z += 0.5;
-          this.el.setAttribute("rotation", diverRotation);
-        }
-        if (diverPosition.y < 2) {
-          diverPosition.y += 0.01;
+      if (e.key === "ArrowDown") {
+        if (diverPosition.z < 20) {
+          diverPosition.z += 0.1;
           this.el.setAttribute("position", diverPosition);
         }
       }
-      if (e.key === "ArrowDown") {
-        if (diverRotation.z > -10) {
-          diverRotation.z -= 0.5;
+      if (e.key === "ArrowUp") {
+        if (diverPosition.z> -10) {
+          diverPosition.z -= 0.1;
+          this.el.setAttribute("position", diverPosition);
+        }
+      }
+      if (e.key === "ArrowRight") {
+        if (diverRotation.y > -360) {
+          diverRotation.y -= 0.5;
           this.el.setAttribute("rotation", diverRotation);
         }
-        if (diverPosition.y > -2) {
-          diverPosition.y -= 0.5;
+        if (diverPosition.x < 20) {
+          diverPosition.x += 0.05;
+          this.el.setAttribute("position", diverPosition);
+        }
+      }
+      if (e.key === "ArrowLeft") {
+        if (diverRotation.y <360) {
+          diverRotation.y += 0.5;
+          this.el.setAttribute("rotation", diverRotation);
+        }
+        if (diverPosition.x > -20) {
+          diverPosition.x -= 0.05;
           this.el.setAttribute("position", diverPosition);
         }
       }
     });
   }
 });
-
